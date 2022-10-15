dc9c698edfe96fad40599d39ae2f285f125ab8d1
tsyganno <tsyganlee@gmail.com>
2022-10-15 21:30:32 +0800

    class MainView

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import MainView
 
 from myblog.views import MainView, post, contact, thanks, signup, signin, search
 
--- myblog/views.py
+++ myblog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views import View
+from .models import Post
 
 
-class MainView(TemplateView):
-    template_name = 'myblog/home.html'
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        return render(request, 'myblog/home.html', context={
+            'posts': posts
+        })
 
 
 def post(request):
