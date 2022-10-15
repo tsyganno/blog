98c0c611420e69fd35c2e1128c6229530d484ed8
tsyganno <tsyganlee@gmail.com>
2022-10-15 20:49:44 +0800

    add Post model

--- myblog/models.py
+++ myblog/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Cat(models.Model):
     name = models.CharField(max_length=200)
     weight_g = models.IntegerField()
+
+
+class Post(models.Model):
+    h1 = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    url = models.SlugField()
+    description = models.TextField()
+    content = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
