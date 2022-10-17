0944ae8fe332402d105a47215385160549773abb
tsyganno <tsyganlee@gmail.com>
2022-10-17 22:05:37 +0800

    test3

--- myblog/views.py
+++ myblog/views.py
@@ -8,10 +8,8 @@ class MainView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all()
         paginator = Paginator(posts, 6)
-
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
-
         return render(request, 'myblog/home.html', context={
             'page_obj': page_obj
         })
