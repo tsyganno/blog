1b174412c370e897a8da1ba9bd4fd409a4b383b1
tsyganno <tsyganlee@gmail.com>
2022-11-01 21:36:58 +0800

    ready

--- myblog/forms.py
+++ myblog/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from myblog.models import Comment
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('text',)
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3
+            }),
+        }
 
 
 class SigUpForm(forms.Form):
--- myblog/views.py
+++ myblog/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 from django.views import View
-from myblog.models import Post
+from myblog.models import Post, Comment
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
-from myblog.forms import SigUpForm, SignInForm, FeedBackForm
+from myblog.forms import SigUpForm, SignInForm, FeedBackForm, CommentForm
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.mail import send_mail, BadHeaderError
 from django.db.models import Q
 from taggit.models import Tag
+
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        common_tags = Post.tag.most_common()
+        last_posts = Post.objects.all().order_by('-id')[:5]
+        comment_form = CommentForm()
+        return render(request, 'myblog/post_detail.html', context={
+            'post': post,
+            'common_tags': common_tags,
+            'last_posts': last_posts,
+            'comment_form': comment_form
+    })
+
+    def post(self, request, slug, *args, **kwargs):
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            text = request.POST['text']
+            username = self.request.user
+            post = get_object_or_404(Post, url=slug)
+            comment = Comment.objects.create(post=post, username=username, text=text)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        return render(request, 'myblog/post_detail.html', context={
+            'comment_form': comment_form
+        })
 
 
 class TagView(View):
@@ -55,18 +81,6 @@ class MainView(View):
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         return render(request, 'myblog/home.html', context={'page_obj': page_obj})
-
-
-class PostDetailView(View):
-    def get(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(Post, url=slug)
-        common_tags = Post.tag.most_common()
-        last_posts = Post.objects.all().order_by('-id')[:5]
-        return render(request, 'myblog/post_detail.html', context={
-            'post': post,
-            'common_tags': common_tags,
-            'last_posts': last_posts
-    })
 
 
 class SignUpView(View):
